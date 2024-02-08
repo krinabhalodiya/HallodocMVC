b4ba3dee2eb3f12629026fa740d7ac0c9db298df
krinabhalodiya <krina.bhalodiya@etatvasoft.com>
2024-02-08 11:41:54 +0530

    patient request

---
+++ HallodocMVC/Controllers/PatientRequestController.cs
@@ -0,0 +1,38 @@
+﻿using HallodocMVC.DataContext;
+using HallodocMVC.DataModels;
+using HallodocMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HallodocMVC.Controllers
+{
+    
+    public class PatientRequestController : Controller
+    {
+        private readonly HalloDocContext _context;
+
+        public PatientRequestController(HalloDocContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View("../Request/PatientRequest");
+        }
+
+        public async Task<IActionResult> Create(ViewDataPatientRequest viewdata)
+        {
+            var A = new AspNetUser();
+            A.Id = "1234";
+                A.Email = viewdata.Email;
+            A.UserName = "Krina";
+                _context.AspNetUsers.Add(A);
+                await _context.SaveChangesAsync();
+            
+
+            return View("../Request/Index");
+        }
+
+    }
+}
---
+++ HallodocMVC/Models/ViewDataPatientRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace HallodocMVC.Models
+{
+    public class ViewDataPatientRequest
+    {
+        public string Id { get; set; }
+       public string Symptoms { get; set; }   
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
