7fe5bd746fa9e836e5b4092233b4d7c4bc8905a3
krinabhalodiya <krina.bhalodiya@etatvasoft.com>
2024-02-22 17:21:14 +0530

    Admin Dashboard Initiate

---
+++ HallodocMVC/Controllers/AdminDashBoardController.cs
@@ -0,0 +1,65 @@
+﻿using HallodocMVC.DataContext;
+using HallodocMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HallodocMVC.Controllers
+{
+    public class AdminDashBoardController : Controller
+    {
+        private readonly HalloDocContext _context;
+
+        public AdminDashBoardController(HalloDocContext context)
+        {
+            _context = context;
+        }
+
+        public enum RequestType
+        {
+            Business = 1,
+            Patient,
+            Family,
+            Concierge
+        }
+
+        public IActionResult Index()
+        {
+            var countRequest = new CountStatusWiseRequestModel
+            {
+                NewRequest = _context.Requests.Where(r => r.Status == 1).Count(),
+                PendingRequest = _context.Requests.Where(r => r.Status == 2).Count(),
+                ActiveRequest = _context.Requests.Where(r => (r.Status == 4 || r.Status == 5)).Count(),
+                ConcludeRequest = _context.Requests.Where(r => r.Status == 6).Count(),
+                ToCloseRequest = _context.Requests.Where(r => (r.Status == 3 || r.Status == 7 || r.Status == 8)).Count(),
+                UnpaidRequest = _context.Requests.Where(r => r.Status == 9).Count(),
+                adminDashboardList = NewRequestData()
+            };
+            return View(countRequest);
+        }
+        public List<AdminDashboardList> NewRequestData()
+        {
+            var list = _context.Requests.Join
+                        (_context.Requestclients,
+                        requestclients => requestclients.Requestid, requests => requests.Requestid,
+                        (requests, requestclients) => new { Request = requests, Requestclient = requestclients }
+                        )
+                        .Where(req => req.Request.Status == 1)
+                        .Select(req => new AdminDashboardList()
+                        {
+                            RequestId = req.Request.Requestid,
+                            PatientName = req.Requestclient.Firstname + " " + req.Requestclient.Lastname,
+                            Email = req.Requestclient.Email,
+                            //DateOfBirth = new DateTime((int)req.Requestclient.Intyear, Convert.ToInt32(req.Requestclient.Strmonth.Trim()), (int)req.Requestclient.Intdate),
+                            RequestTypeId = req.Request.Requesttypeid,
+                            Requestor = req.Request.Firstname + " " + req.Request.Lastname,
+                            RequestedDate = req.Request.Createddate,
+                            PatientPhoneNumber = req.Requestclient.Phonenumber,
+                            RequestorPhoneNumber = req.Request.Phonenumber,
+                            Notes = req.Requestclient.Notes,
+                            Address = req.Requestclient.Address + " " + req.Requestclient.Street + " " + req.Requestclient.City + " " + req.Requestclient.State + " " + req.Requestclient.Zipcode
+                        })
+                        .OrderByDescending(req => req.RequestedDate)
+                        .ToList();
+            return list;
+        }
+    }
+}
---
+++ HallodocMVC/Models/CountStatusWiseRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HallodocMVC.Models
+{
+    public class CountStatusWiseRequestModel
+    {
+        public int NewRequest { get; set; }
+        public int PendingRequest { get; set; }
+        public int ActiveRequest { get; set; }
+        public int ConcludeRequest { get; set; }
+        public int ToCloseRequest { get; set; }
+        public int UnpaidRequest { get; set; }
+        public List<AdminDashboardList> adminDashboardList { get; set; }
+    }
+}
